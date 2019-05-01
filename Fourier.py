f464674061a0e2557431ff2cbaf7815b5079184f
Ana Bello <anamariabellopena@MacBook-Air-de-Ana.local>
2019-05-01 17:50:13 -0500

    Realizacion de graficas, mejorar espacio de subplots

--- Fourier.py
+++ Fourier.py
@@ -1,18 +1,35 @@
 import numpy as np 
 import matplotlib.pylab as plt
-from scipy import fft
-from scipy import fftfreq
-import mat
+from scipy import fftpack
+from scipy.fftpack import fft, fftfreq
+
 
 #Almacenamiento de datos signal y signalSuma(suma de dos ondas).
 
-datossignal= np.genfromtxt("signal.dat", delimiter=",")
-datossuma= np.genfromtxt("signalSuma.dat", delimiter=",")
+datossignal= np.genfromtxt("signal.dat")
+datossuma= np.genfromtxt("signalSuma.dat")
 
-t= datos[:,0]
+t= datossignal[:,0]
+ts=datossuma[:,0]
 senial = datossignal[:,1]
 ssuma= datossuma[:,1]
 
+#Grafica con dos subplots uno con los datos de signal y otro con el de suma.
 
+plt.figure()
+
+plt.subplot(2,1,1)
+plt.plot(t,senial,'c')
+plt.title("Senal 1")
+plt.xlabel("t(s)")
+plt.ylabel("senial")
+
+plt.subplot(2,1,2)
+plt.plot(t,ssuma, 'g')
+plt.title("Suma de seniales")
+plt.xlabel("t(s)")
+plt.ylabel("senial")
+
+plt.savefig("Seniales.pdf")
 
 
